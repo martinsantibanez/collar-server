ca35572461c7631685a328b88ab80def7e5f619b
Martín Santibáñez <martin.santibanez.a@gmail.com>
2018-10-22 02:52:44 -0300

    initial server random data sockets

---
+++ index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+
+var app = express();
+var server = app.listen(4000, () => { //Start the server, listening on port 4000.
+    console.log("Listening to requests on port 4000...");
+})
+
+var io = require('socket.io')(server); //Bind socket.io to our express server.
+
+app.use(express.static('public')); //Send index.html page on GET /
+
+setInterval(function(){
+    console.log('emitting');
+    var d = new Date();
+    io.sockets.emit('DATA', {
+        x: d,
+        // x: Math.random(),
+        y: Math.random(),
+    });
+}, 300);
+
+io.on('connection', (socket) => {
+    console.log("Someone connected."); //show a log as a new client connects.
+})
